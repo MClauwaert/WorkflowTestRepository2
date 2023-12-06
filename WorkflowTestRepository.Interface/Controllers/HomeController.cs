e5fc564d0f1364dbc03642b333eac4cc52001def
Maarten Clauwaert <91329133+MClauwaert@users.noreply.github.com>
2023-12-06 14:37:46 +0100

    Yml check-in

---
+++ WorkflowTestRepository.Interface/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkflowTestRepository.Interface.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World!");
+    }
+}
